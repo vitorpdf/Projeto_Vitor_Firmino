6fe4333e1b4c33b819b6dfbc74e5abba8b2d3709
Vitor Firmino <vitorgus3@gmail.com>
2025-06-14 20:09:13 -0300

    Adicionado método MD3() localmente

--- D.java
+++ D.java
@@ -26,4 +26,9 @@ public class D {
     public void MD2() {
         System.out.println("Método MD2()");
     }
+
+     public void MD3() {
+        System.out.println("Método MD3() - Alteração local");
+    }
+
 }
