dd922ebf5f21821a0c747064d6645988558e8011
Vitor Firmino <vitorgus3@gmail.com>
2025-06-14 21:53:38 -0300

    Merge da classe D com MD3 e MD4

--- D.java
+++ D.java
@@ -26,4 +26,11 @@ public class D {
     public void MD2() {
         System.out.println("Método MD2()");
     }
+    public void MD3() {
+        System.out.println("Método MD3() - Alteração local");
+    }
+
+    public void MD4() {
+        System.out.println("Método MD4() - Alteração no GitHub");
+    }
 }
